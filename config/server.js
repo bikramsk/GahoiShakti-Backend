1da2157ed32c5ccced8bcddc1ff32ed6d0021356
bikramsk <bikramkanyal@gmail.com>
2025-06-15 08:03:41 +0530

    updated configs

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: env('PUBLIC_URL', 'https://api.gahoishakti.in'), 
+  url: env('PUBLIC_URL', 'https://api2.gahoishakti.in'), 
   host: '0.0.0.0', 
   port: env.int('PORT', 1337),
   app: {
