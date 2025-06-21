18735521e096f36a44d825eb72bd9b13b7ce0bb8
bikramsk <bikramkanyal@gmail.com>
2025-06-21 20:30:01 +0530

    added gallery schema and configs

--- config/server.js
+++ config/server.js
@@ -13,6 +13,21 @@ module.exports = ({ env }) => ({
 });
 
 
+// module.exports = ({ env }) => ({
+//   url: env('NODE_ENV') === 'development' 
+//     ? 'http://localhost:1340'
+//     : 'https://admin.gahoishakti.in',
+//   host: '0.0.0.0', 
+//   port: env.int('PORT', 1340),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   admin: {
+//     url: '/admin', 
+//     serveAdminPanel: true,
+//   },
+// });
+
 
 // module.exports = ({ env }) => ({
 //   host: env('HOST', '0.0.0.0'),
